def05c9f532d5fbb35df48578feca35400b03d6d
joeyappelhans <9jappelhans@stpatrick.org>
2016-12-12 13:59:22 -0600

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,13 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+  constructor(itemList,itemQuantity){
+    this.itemList=itemList;
+    this.itemQuantity=itemQuantity;
+  }
+  //Type the instance functions below this comment.
+  addItem(itemList,itemQuantity){
+    this.itemList=i;
+    this.itemQuantity=q;
+  }
+
+}
