0fc5fae59e0d737b620f65ba891831ced17f5c56
joeyappelhans <9jappelhans@stpatrick.org>
2016-12-15 13:42:32 -0600

    finished

--- final-master/cart.js
+++ final-master/cart.js
@@ -1,28 +1,13 @@
 class Cart{
   //What is the first part of every class? Type it below.
-  constructor(i,q){
+  constructor(itemList,itemQuantity){
+    this.itemList=itemList;
+    this.itemQuantity=itemQuantity;
+  }
+  //Type the instance functions below this comment.
+  addItem(itemList,itemQuantity){
     this.itemList=i;
     this.itemQuantity=q;
   }
-  //Type the instance functions below this comment.
-  addItem(i,q){
-    this.itemList.push(i);
-    this.itemQuantity.push(q);
-  }
-  totalCart(){
-    var total=t;
-    for(item=i; i<=itemList.length; i++){
-      total += this.itemList[a].price*this.itemQuantity[a];
-    }
-    return total
-  }
-  subCart(days){
-    let subCart = new Cart([],[]);
-    for(b=0;b>this.itemList.length;b++){
-      if(this.itemList[b].shipping == days){
-        subCart.push([b]);
-      }
-    }
-    return subCart;
-  }
+
 }
