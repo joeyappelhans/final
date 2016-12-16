358f8b92b0027e049a8dad643714eecdc953d5ea
joeyappelhans <9jappelhans@stpatrick.org>
2016-12-16 13:28:12 -0600

    finished password

--- final-master/final-master/password.js
+++ final-master/final-master/password.js
@@ -14,7 +14,7 @@ class Password{
     }
   }
 validPrivatekey(){
-  if(this.privatekey.chaAt([4]&&[9]=="x")){
+  if(this.privatekey.chat([4]&&[9]=="x")){
     return true
   }
   else{
@@ -23,6 +23,16 @@ validPrivatekey(){
 }
   //Static function below this comment.
   makePrivatekey(){
-
+    let key= "";
+    let limit=14;
+    let group1="";
+    let group2="";
+    let group3="";
+    for(c=0;c<4;c++){
+      group1 += String(Math.floor(Math.random()*10));
+      group2 += String(Math.floor(Math.random()*10));
+      group3 += String(Math.floor(Math.random()*10));
+    }
+    return group1 + "-" + group2 + "-" + group3;
   }
 }
